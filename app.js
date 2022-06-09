7e9192d153f3b4c0f3b819d0f2f4bdd30c8057a0
Jelani2022 <jtwash2@gmail.com>
2022-06-09 10:50:20 -0400

    changes

--- app.js
+++ app.js
@@ -13,27 +13,33 @@ console.log("EXERCISE 2:\n==========\n");
 let fiz = "fizz"
 let buz = "buzz"
 for (let i = 1; i <= 100; i++){
-    if (i%3==0){
-        console.log(fiz)
-    } if (i%5==0){
-        console.log(buz)
-    } if (i%3==0 , i%5==0){
+    if (i%3==0 , i%5==0){
         console.log(fiz + buz)
-    }
+    } else if (i%3==0){
+        console.log(fiz)
+    } else if (i%5==0){
+        console.log(buz)
+    } 
 }
 // Exercise 3
-
+let i = 1;  
+while (i < 100){
+    if (i%2 !==0){
+        console.log(i)
+    }
+ i++
+}
 
 
 
 // Exercise 4
 let value = Math.round((Math.random() * 500)); // creates a random number between 0 and 500
-let n = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
+let N = Math.round(Math.random() * (500 - 100) + 100); // creates a random number between 100 and 500
 let found = "Found Value!"
 let nope = "Did not find value."
-for (){
 
-}
+
+
 // Exercise 5
 let fizzDivisor = Math.round(Math.random() * (1 - 10) + 1);
 let buzzDivisor = Math.round(Math.random() * (1 - 10) + 1);
